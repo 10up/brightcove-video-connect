c381f5202cd5c62d261c6f42662e1879dc48accc
Chris Wiegman <chris.wiegman@10up.com>
2015-12-23 11:26:44 -0500

    Let PhpStorm auto-cleanup code

--- includes/admin/class-bc-admin-playlists-page.php
+++ includes/admin/class-bc-admin-playlists-page.php
@@ -3,6 +3,7 @@
 class BC_Admin_Playlists_Page {
 
 	public function __construct() {
+
 		add_action( 'brightcove/admin/playlists_page', array( $this, 'render' ) );
 		add_action( 'admin_notices', array( $this, 'validate_players' ) );
 	}
@@ -11,6 +12,7 @@ class BC_Admin_Playlists_Page {
 	 * Generates an HTML table with all configured sources
 	 */
 	public function render() {
+
 		/*
 		 * This is all handled by the Backbone app,
 		 * refer to app.js->load() for the hook into
@@ -18,16 +20,19 @@ class BC_Admin_Playlists_Page {
 		 */
 		?>
 		<span class="wrap">
-			<h2><img class="bc-page-icon" src="<?php echo esc_url( BRIGHTCOVE_URL . 'images/menu-icon.svg' ); ?>"> <?php esc_html_e( 'Brightcove Playlists', 'brightcove' ); ?></h2>
+			<h2>
+				<img class="bc-page-icon" src="<?php echo esc_url( BRIGHTCOVE_URL . 'images/menu-icon.svg' ); ?>"> <?php esc_html_e( 'Brightcove Playlists', 'brightcove' ); ?>
+			</h2>
 		</span>
 		<div class="brightcove-media-playlists"></div>
-	<?php
+		<?php
 	}
 
 	/**
 	 * Ensure there is a playlist capable player available
 	 */
 	public function validate_players() {
+
 		global $pagenow, $plugin_page;
 
 		// only on admin.php?page=page-brightcove-playlists
@@ -39,9 +44,11 @@ class BC_Admin_Playlists_Page {
 		$players    = $player_api->player_list_playlist_enabled();
 		if ( is_wp_error( $players ) || ! is_array( $players ) || $players['item_count'] < 1 ) {
 			BC_Utility::admin_notice_messages( array(
-					array( 'message' => __( 'A specified Source does not have a playlist capable player <a href="https://studio.brightcove.com/products/videocloud/players/">configured</a>. Make sure there is at least one player with "Display Playlist" enabled.', 'brightcove' ),
-							'type' => 'error' )
-				)
+				                                   array(
+					                                   'message' => __( 'A specified Source does not have a playlist capable player <a href="https://studio.brightcove.com/products/videocloud/players/">configured</a>. Make sure there is at least one player with "Display Playlist" enabled.', 'brightcove' ),
+					                                   'type'    => 'error',
+				                                   ),
+			                                   )
 			);
 		}
 	}
