32f66477ffb06b034ab4f828b6ae33920db0f858
Morgan Newcomb <manewc@gmail.com>
2016-08-04 16:13:43 -0400

    filterable localized variable to set the posts per page in media query

--- includes/class-bc-setup.php
+++ includes/class-bc-setup.php
@@ -213,6 +213,7 @@ class BC_Setup {
 			'str_selectfile' => esc_html__( 'Select File', 'brightcove' ),
 			'str_useremote'  => esc_html__( 'Use a remote file instead', 'brightcove' ),
 			'str_apifailure'  => esc_html__( "Sorry! We weren't able to reach the Brightcove API even after trying a few times. Please try refreshing the page.", 'brightcove' ),
+			'posts_per_page' => absint( apply_filters( 'brightcove_posts_per_page', 100 ) ),
 		);
 
 		wp_register_script( 'brightcove', '//sadmin.brightcove.com/js/BrightcoveExperiences.js' );
