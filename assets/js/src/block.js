5a6b1eb60122fd5fcb34e88031150b0a542a7fd6
Darin Kotter <darin.kotter@my.eitc.edu>
2018-07-09 13:43:43 -0600

    Add support for the experience shortcodes in our block. Change up slightly how we handle the block display when a user doesn't have proper permissions.

--- assets/js/src/block.js
+++ assets/js/src/block.js
@@ -1,32 +1,20 @@
-/* global wp, bctiny */
+/* global wp, bctiny, bcBlock */
 
-( function( blocks, element, components ) {
+( function( blocks, element, components, editor ) {
 	var el = element.createElement,
 		registerBlockType = blocks.registerBlockType,
 		Placeholder = components.Placeholder,
-		BlockControls = blocks.BlockControls,
-		IconButton = components.IconButton;
+		BlockControls = editor.BlockControls,
+		IconButton = components.IconButton,
+		userPermission = !!bcBlock.userPermission;
 
 	registerBlockType( 'bc/brightcove', {
 		title: 'Brightcove',
 		description: 'The Brightcove block allows you to embed videos from Brightcove.',
 		icon: 'video-alt3',
-		category: 'common', // embed?
-		attributes: {
-			account_id: {
-				type: 'int'
-			},
-			player_id: {
-				type: 'int'
-			},
-			video_id: {
-				type: 'int'
-			},
-			playlist_id: {
-				type: 'int'
-			}
-		},
+		category: 'common',
 		supports: {
+			inserter: userPermission,
 			html: false
 		},
 
@@ -39,6 +27,15 @@
 			var playerId = props.attributes.player_id || '';
 			var videoId = props.attributes.video_id || '';
 			var playlistId = props.attributes.playlist_id || '';
+			var experienceId = props.attributes.experience_id || '';
+			var videoIds = props.attributes.video_ids || '';
+			var height = props.attributes.height || '';
+			var width = props.attributes.width || '';
+			var minWidth = props.attributes.min_width || '';
+			var maxWidth = props.attributes.max_width || '';
+			var paddingTop = props.attributes.padding_top || '';
+			var autoplay = props.attributes.autoplay || '';
+			var embed = props.attributes.embed || '';
 
 			// Sanitize the IDs we need
 			var sanitizeIds = function( id ) {
@@ -56,22 +53,43 @@
 			var onSelectVideo = function() {
 				var btn = document.getElementById( target );
 				var attrs = wp.shortcode.attrs( btn.value );
+				var setAttrs = {
+					account_id: sanitizeIds( attrs.named.account_id ),
+					player_id: '',
+					video_id: '',
+					playlist_id: '',
+					experience_id: '',
+					video_ids: '',
+					height: attrs.named.height,
+					width: attrs.named.width,
+					min_width: attrs.named.min_width,
+					max_width: attrs.named.max_width,
+					padding_top: '',
+					autoplay: '',
+					embed: attrs.named.embed
+				};
 
 				if ( '[bc_video' === attrs.numeric[0] ) {
-					props.setAttributes( {
-						account_id: sanitizeIds( attrs.named.account_id ),
-						player_id: attrs.named.player_id,
-						video_id: sanitizeIds( attrs.named.video_id ),
-						playlist_id: ''
-					} );
+					setAttrs.player_id = attrs.named.player_id;
+					setAttrs.video_id = sanitizeIds( attrs.named.video_id );
+					setAttrs.autoplay = attrs.named.autoplay;
+					setAttrs.padding_top = attrs.named.padding_top;
 				} else if ( '[bc_playlist' === attrs.numeric[0] ) {
-					props.setAttributes( {
-						account_id: sanitizeIds( attrs.named.account_id ),
-						player_id: attrs.named.player_id,
-						video_id: '',
-						playlist_id: sanitizeIds( attrs.named.playlist_id )
-					} );
+					setAttrs.player_id = attrs.named.player_id;
+					setAttrs.playlist_id = sanitizeIds( attrs.named.playlist_id );
+					setAttrs.autoplay = attrs.named.autoplay;
+					setAttrs.padding_top = attrs.named.padding_top;
+				} else if ( '[bc_experience' === attrs.numeric[0] ) {
+					setAttrs.experience_id = attrs.named.experience_id;
+
+					if ( 'undefined' !== typeof attrs.named.video_ids ) {
+						setAttrs.video_ids = sanitizeIds( attrs.named.video_ids );
+					} else {
+						setAttrs.playlist_id = sanitizeIds( attrs.named.playlist_id );
+					}
 				}
+
+				props.setAttributes( setAttrs );
 			};
 
 			// Listen for a change event on our hidden input
@@ -86,7 +104,7 @@
 						IconButton,
 						{
 							className: 'brightcove-add-media components-icon-button components-toolbar__control',
-							label: videoId.length ? 'Change Video' : 'Change Playlist',
+							label: videoId.playlist_id ? 'Change Playlist' : 'Change Video',
 							icon: 'edit',
 							'data-target': '#' + target
 						}
@@ -95,40 +113,56 @@
 			);
 
 			// If no video has been selected yet, show the selection view
-			if ( ! accountId.length && ! playerId.length && ( ! videoId.length || ! playlistId.length ) ) {
+			if ( ! accountId.length && ( ! playerId.length || ! experienceId.length ) && ( ! videoId.length || ! playlistId.length || videoIds.length ) ) {
 				return el( Placeholder, {
 					icon: 'media-video',
 					label: 'Brightcove',
-					instructions: 'Select a video file or playlist from your Brightcove library',
+					instructions: userPermission ? 'Select a video file or playlist from your Brightcove library' : 'You don\'t have permissions to add Brightcove videos.',
 					children: [
-						el( 'button', { className: 'brightcove-add-media button button-large', 'data-target': '#' + target, key: 'button' }, 'Brightcove Media' ),
+						userPermission ? el( 'button', { className: 'brightcove-add-media button button-large', 'data-target': '#' + target, key: 'button' }, 'Brightcove Media' ) : '',
 						el( 'input', { id: target, hidden: true, key: 'input' } )
 					]
 				} );
 
-			// Otherwise render the iframe
+			// Otherwise render the shortcode
 			} else {
 				var src = '';
 
-				if ( videoId.length ) {
+				if ( experienceId.length ) {
+					var urlAttrs = '';
+					if ( videoIds.length ) {
+						urlAttrs = 'videoIds=' + videoIds;
+					} else {
+						urlAttrs = 'playlistId=' + playlistId;
+					}
+					src = '//players.brightcove.net/' + accountId + '/experience_' + experienceId + '/index.html?' + urlAttrs;
+				} else if ( videoId.length ) {
 					src = '//players.brightcove.net/' + accountId + '/' + playerId + '_default/index.html?videoId=' + videoId;
 				} else {
 					playerId = bctiny.playlistEnabledPlayers[ accountId ][0] || 'default';
 					src = '//players.brightcove.net/' + accountId + '/' + playerId + '_default/index.html?playlistId=' + playlistId;
 				}
 
+				if ( 'undefined' === typeof height ) {
+					height = 250;
+				}
+
+				if ( 'undefined' === typeof width ) {
+					width = 500;
+				}
+
 				return [
-					controls,
-					el( 'iframe', { src: src, style: { height: 250, width: 500, display: 'block', margin: '0 auto' }, key: 'iframe' } ),
+					userPermission ? controls : '',
+					el( 'iframe', { src: src, style: { height: height, width: width, display: 'block', margin: '0 auto' }, allowFullScreen: true, key: 'iframe' } ),
 					el( 'input', { id: target, hidden: true, key: 'input' } )
 				];
 			}
 		},
 
-		save: function( props ) {
+		save: function() {
 			return null;
 		}
 
 	} );
 
-} )( window.wp.blocks, window.wp.element, window.wp.components );
+} )( window.wp.blocks, window.wp.element, window.wp.components, window.wp.editor );
