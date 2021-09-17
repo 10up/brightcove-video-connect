8026e2821f1f50908ef293216ae84ff0698a02b2
Ramon <Rahmon@users.noreply.github.com>
2021-09-17 15:07:05 -0300

    Add controls to the sidebar of the block

--- assets/js/src/block.js
+++ assets/js/src/block.js
@@ -1,12 +1,13 @@
 /* global wp, bctiny, bcBlock */
-
 (function (blocks, element, components, editor) {
 	var el = element.createElement,
 		registerBlockType = blocks.registerBlockType,
 		Placeholder = components.Placeholder,
 		BlockControls = editor.BlockControls,
 		IconButton = components.IconButton,
-		userPermission = !!bcBlock.userPermission;
+		userPermission = !!bcBlock.userPermission,
+		InspectorControls = wp.blockEditor.InspectorControls,
+		TextControl = components.TextControl;
 
 	registerBlockType('bc/brightcove', {
 		title: 'Brightcove',
@@ -19,6 +20,8 @@
 		},
 
 		edit: function (props) {
+			const [isHeightFieldDisabled, setIsHeightFieldDisabled] = element.useState(true);
+
 			// Set the field we want to target
 			var target = 'brightcove-' + props.clientId;
 
@@ -29,15 +32,92 @@
 			var playlistId = props.attributes.playlist_id || '';
 			var experienceId = props.attributes.experience_id || '';
 			var videoIds = props.attributes.video_ids || '';
-			var height = props.attributes.height || '';
-			var width = props.attributes.width || '';
 			var minWidth = props.attributes.min_width || '';
-			var maxWidth = props.attributes.max_width || '';
 			var paddingTop = props.attributes.padding_top || '';
 			var autoplay = props.attributes.autoplay || '';
 			var playsinline = props.attributes.playsinline || '';
 			var pictureinpicture = props.attributes.picture_in_picture || '';
 			var embed = props.attributes.embed || '';
+			var mute = props.attributes.mute || '';
+			var sizing = props.attributes.sizing || 'responsive';
+			var aspectRatio = props.attributes.aspect_ratio || '16:9';
+			var width =
+				sizing === 'fixed'
+					? props.attributes.width?.replace(/[^0-9]/g, '') || '640'
+					: props.attributes.max_width?.replace(/[^0-9]/g, '') || '640';
+
+			var height = props.attributes.height?.replace(/[^0-9]/g, '');
+
+			const maxHeight = props.attributes.max_height?.replace(/[^0-9]/g, '') || height;
+			const maxWidth = props.attributes.max_width?.replace(/[^0-9]/g, '') || width;
+
+			const account = _.findWhere(wpbc.preload.accounts, {
+				account_id: accountId,
+			});
+			const accountName = account.account_name;
+
+			element.useEffect(() => {
+				if (aspectRatio === 'custom') {
+					setIsHeightFieldDisabled(false);
+				} else {
+					setIsHeightFieldDisabled(true);
+				}
+			}, [aspectRatio]);
+
+			element.useEffect(() => {
+				if (!props.attributes.max_height) {
+					const height =
+						props.attributes.sizing === 'responsive' ? '100%' : props.attributes.height;
+					props.setAttributes({
+						...props.attributes,
+						max_height: props.attributes.height,
+						height,
+					});
+				}
+			}, []);
+
+			element.useEffect(() => {
+				if (
+					props.attributes.sizing === 'responsive' &&
+					props.attributes.height !== '100%'
+				) {
+					props.setAttributes({
+						...props.attributes,
+						height: '100%',
+						max_height: `${props.attributes.height?.replace(/[^0-9]/g, '')}px`,
+					});
+				}
+			}, [props.attributes.height, props.attributes.sizing]);
+
+			element.useEffect(() => {
+				if (
+					props.attributes.sizing === 'fixed' &&
+					(props.attributes.width === '100%' || props.attributes.height === '100%')
+				) {
+					props.setAttributes({
+						...props.attributes,
+						width:
+							props.attributes.width === '100%'
+								? props.attributes.max_width
+								: undefined,
+						height:
+							props.attributes.height === '100%'
+								? props.attributes.max_height
+								: undefined,
+					});
+				}
+			}, [props.attributes.width, props.attributes.sizing]);
+
+			element.useEffect(() => {
+				if (aspectRatio === 'custom') {
+					const padding_top =
+						typeof maxHeight === 'number' && typeof maxWidth === 'number'
+							? `${(maxHeight / maxWidth) * 100}%`
+							: '56%';
+
+					props.setAttributes({ ...props.attributes, padding_top });
+				}
+			}, [maxWidth, maxHeight, aspectRatio]);
 
 			// Sanitize the IDs we need
 			var sanitizeIds = function (id) {
@@ -197,6 +277,20 @@
 				width = 500;
 			}
 
+			const players = wpbc.players[accountId]
+				.filter((player) => {
+					return playlistId ? player.is_playlist : !player.is_playlist;
+				})
+				.reduce((previousValue, currentValue) => {
+					return [
+						...previousValue,
+						{
+							label: currentValue.name,
+							value: currentValue.id,
+						},
+					];
+				}, []);
+
 			return [
 				userPermission ? controls : '',
 				el('iframe', {
@@ -206,6 +300,182 @@
 					key: 'iframe',
 				}),
 				el('input', { id: target, hidden: true, key: 'input' }),
+				el(
+					InspectorControls,
+					{ key: 'inspector' }, // Display the block options in the inspector panel.
+					el(
+						components.PanelBody,
+						{
+							title: 'Settings',
+							initialOpen: true,
+						},
+						el('p', {}, `Source: ${accountName}`),
+						el('p', {}, `Video ID: ${videoId}`),
+						el(components.SelectControl, {
+							label: 'Video Player',
+							value: playerId,
+							options: players,
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									player_id: value,
+								});
+							},
+						}),
+						el(components.CheckboxControl, {
+							label: 'Autoplay',
+							checked: autoplay,
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									autoplay: value && 'autoplay',
+								});
+							},
+						}),
+						el(components.CheckboxControl, {
+							label: 'Mute',
+							checked: mute,
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									mute: value && 'muted',
+								});
+							},
+						}),
+						el(components.CheckboxControl, {
+							label: 'Plays in line',
+							checked: playsinline,
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									playsinline: value && 'playsinline',
+								});
+							},
+						}),
+						el(components.CheckboxControl, {
+							label: 'Enable Picture in Picturee',
+							checked: pictureinpicture,
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									picture_in_picture: value && 'pictureinpicture',
+								});
+							},
+						}),
+						el(components.RadioControl, {
+							label: 'Embed Style',
+							selected: embed,
+							options: [
+								{ label: 'JavaScript', value: 'in-page' },
+								{ label: 'iFrame', value: 'iframe' },
+							],
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									embed: value,
+								});
+							},
+						}),
+						el(components.RadioControl, {
+							label: 'Sizing',
+							selected: sizing,
+							options: [
+								{ label: 'Responsive', value: 'responsive' },
+								{ label: 'Fixed', value: 'fixed' },
+							],
+							onChange: function (value) {
+								props.setAttributes({
+									...props.attributes,
+									sizing: value,
+								});
+							},
+						}),
+						el(components.SelectControl, {
+							label: 'Aspect Ratio',
+							value: aspectRatio,
+							options: [
+								{
+									label: '16:9',
+									value: '16:9',
+								},
+								{
+									label: '4:3',
+									value: '4:3',
+								},
+								{
+									label: 'Custom',
+									value: 'custom',
+								},
+							],
+							onChange: function (value) {
+								let height;
+								let padding_top;
+
+								if (value === '16:9') {
+									height = '360px';
+									padding_top = '56%';
+								} else if (value === '4:3') {
+									height = '480px';
+									padding_top = '75%';
+								} else {
+									height = `${maxHeight}px`;
+
+									padding_top =
+										typeof maxHeight === 'number' &&
+										typeof maxWidth === 'number'
+											? `${(maxHeight / maxWidth) * 100}%`
+											: '56%';
+								}
+
+								props.setAttributes({
+									...props.attributes,
+									aspect_ratio: value,
+									height,
+									padding_top,
+								});
+							},
+						}),
+						el(components.TextControl, {
+							label: 'Width',
+							type: 'number',
+							value: width,
+							onChange: function (value) {
+								let width = `${value}px`;
+								const max_width = width;
+
+								if (sizing === 'responsive') {
+									width = '100%';
+								}
+
+								props.setAttributes({
+									...props.attributes,
+									width,
+									max_width,
+								});
+							},
+						}),
+						el(components.TextControl, {
+							label: 'Height',
+							type: 'number',
+							value: sizing === 'fixed' ? height : maxHeight,
+							disabled: isHeightFieldDisabled,
+							onChange: function (value) {
+								let height = `${value}px`;
+								const max_height = height;
+
+								if (sizing === 'responsive') {
+									height = '100%';
+								}
+
+								props.setAttributes({
+									...props.attributes,
+									height,
+									max_height,
+								});
+							},
+						}),
+					),
+				),
 			];
 		},
 
