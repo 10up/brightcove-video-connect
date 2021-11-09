d6fad7cab86d6c6506aa124eb79a7ee0390a8974
Ramon <Rahmon@users.noreply.github.com>
2021-11-09 10:47:53 -0300

    Add get_experiences_by_account_id function

--- includes/api/class-bc-experiences-api.php
+++ includes/api/class-bc-experiences-api.php
@@ -60,6 +60,30 @@ class BC_Experiences_API extends BC_API {
 	}
 
 	/**
+	 * Fetches experiences by account id from Experiences API.
+	 *
+	 * @since 2.4.0
+	 *
+	 * @return mixed
+	 */
+	public function get_experiences_by_account_id( $account_id ) {
+		$account_experiences = array();
+
+		if ( ! is_numeric( $account_id ) ) {
+			return [];
+		}
+
+		$url                 = esc_url_raw( self::BASE_URL . $account_id . '/experiences/' );
+		$account_experiences = $this->send_request( $url );
+
+		if ( is_wp_error( $account_experiences ) ) {
+			return [];
+		}
+
+		return apply_filters( 'brightcove_get_experiences_by_account_id', $account_experiences, $account_id );
+	}
+
+	/**
 	 * Fetch a In-Page experience
 	 *
 	 * @param string $in_page_expirence_id ID of In-Page experience
