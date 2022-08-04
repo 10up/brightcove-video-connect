ef7f50c7be30710f8cc191485b1f96714c78911c
Oscar Sanchez S <oscar@oscarssanchez.com>
2022-08-04 15:18:27 -0500

    includes/class-bc-tags.php phpcs fixes

--- includes/class-bc-tags.php
+++ includes/class-bc-tags.php
@@ -1,16 +1,36 @@
 <?php
+/**
+ * BC_Tags class file.
+ *
+ * @package Brightcove_Video_Connect
+ */
 
+/**
+ * BC_Tags class.
+ */
 class BC_Tags {
 
+	/**
+	 * Key to identify tags
+	 *
+	 * @var string
+	 */
 	private $key = '_brightcove_tags';
 
-	public function __construct() {
-	}
-
+	/**
+	 * Gets tags from option
+	 *
+	 * @return false|mixed|void
+	 */
 	public function get_tags() {
 		return get_option( $this->key, array() );
 	}
 
+	/**
+	 * Adds tags to our option
+	 *
+	 * @param array $new_tags new tags to add
+	 */
 	public function add_tags( $new_tags ) {
 		$existing_tags = $this->get_tags();
 		$merged_tags   = array_unique( array_merge( $existing_tags, $new_tags ) );
