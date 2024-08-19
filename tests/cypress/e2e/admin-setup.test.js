618c102110b16e6c26e211de3ff5fa974067c3cf
Burhan Nasir <burhan.nasir@10up.com>
2024-08-19 15:07:03 +0500

    Update @wordpress/env package
    Update node
    
    Update sh script
    
    Update config
    
    Update config
    
    Update command
    
    Update config
    
    Update config
    
    Update config
    
    Update package
    
    Update package
    
    Update package
    
    Update package
    
    Update test
    
    Update test
    
    Update test
    
    Update test

--- tests/cypress/e2e/admin-setup.test.js
+++ tests/cypress/e2e/admin-setup.test.js
@@ -1,24 +1,24 @@
 describe("Plugin Setup Tests", () => {
   before(() => {
-    cy.login();
     // We start fresh before running our tests.
     cy.deleteBrightcoveOptions();
   });
 
-  it("Brightcove Video Connect can be activated and deactivated", () => {
+  beforeEach(() => {
     cy.login();
+  });
+
+  it("Brightcove Video Connect can be activated and deactivated", () => {
     cy.activatePlugin("brightcove-video-connect");
     cy.deactivatePlugin("brightcove-video-connect");
   });
 
   it("Display admin notice asking to configure the plugin displays when activating the plugin for the first time", () => {
-    cy.login();
     cy.activatePlugin("brightcove-video-connect");
     cy.get('.configure-brightcove').should('exist');
   });
 
   it( "Can successfully connect to Brightcove", () => {
-    cy.login();
     cy.visitAdminPage('?page=page-brightcove-edit-source');
     cy.get('#source-name').type('Cypress');
     cy.get('#source-account-id').type(Cypress.env('brightcoveAccountId'));
